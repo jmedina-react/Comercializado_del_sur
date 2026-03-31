ce601912b8d01b9f4557f01c39c4418d642ca4d0
Josue Medina Telada <josuemedinatelada@gmail.com>
2026-03-31 00:53:26 -0500

    Implement data loading and null removal
    Added data loading and cleaning steps in LIMPIEZA.py.

---
+++ LIMPIEZA.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+
+# 1. CARGA Y LIMPIEZA DE DATOS
+# 1.1 Cargamos el dataset
+df = pd.read_csv("dataset.txt")
+df.isnull().sum()  # Verificamos nulos en el dataset "Total Price"
+
+# 1.2 Eliminacion de nulos
+df = df.dropna()  # Eliminamos filas con nulos
+df.isnull().sum()
